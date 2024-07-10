9339e89cf9b3bdb43f7036f7dfe0899c1a9967fb
LifeHashed <arpon.skhan@gmail.com>
2024-07-10 16:33:09 +0530

    Less go

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Version Control System",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Project 1",
+    des: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "Project 2",
+    des: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    img: "/p2.svg",
+    //iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Project 3",
+    des: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Project 4",
+    des: "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    img: "/p4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Arpon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arpon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arpon is the ideal partner.",
+    name: "Subhajit Pathak",
+    title: "Director of TopFlow Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Arpon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arpon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arpon is the ideal partner.",
+    name: "Subhajit Pathak",
+    title: "Director of TopFlow Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Arpon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arpon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arpon is the ideal partner.",
+    name: "Subhajit Pathak",
+    title: "Director of TopFlow Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Arpon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arpon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arpon is the ideal partner.",
+    name: "Subhajit Pathak",
+    title: "Director of TopFlow Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Arpon was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Arpon's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Arpon is the ideal partner.",
+    name: "Subhajit Pathak",
+    title: "Director of TopFlow Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "React and NEXT.js Developer",
+    desc: "Assisted in the development of a web-based platform using React.js and NEXT.js enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev",
+    desc: "Designed and developed mobile app for both Android platforms using Android Studio.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project-Ayurjyoti",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Backend Developer",
+    desc: "well versed in PSQL, MongoDB, and Firebase, I have experience in developing and maintaining backend services.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/LifeHashed",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/home?lang=en"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/arpon-sadhukhan-b48a60268/"
+  },
+];
